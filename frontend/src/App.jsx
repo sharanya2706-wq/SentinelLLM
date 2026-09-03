3837304473b097f4d17c67194f1cfbf958ef19f6
Sharanya Vasavarla <sharanyavasavarla276@gmail.com>
2026-09-03 18:03:47 +0530

    Update backend port to 7860

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import {
 
 import "./App.css";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "http://127.0.0.1:7860";
 
 function App() {
   const [activePage, setActivePage] =
